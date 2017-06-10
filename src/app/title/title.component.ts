73c77e344e3ab5c4ae150d273d31f7621269543c
ryan10328 <ryan10328@gmail.com>
2017-06-10 10:02:04 +0800

    任務 04：套用內嵌繫結 (Interpolation)

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./title.component.css']
 })
 export class TitleComponent implements OnInit {
+  title = 'todos';
 
   constructor() { }
 
