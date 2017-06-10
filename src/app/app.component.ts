bcaf22a419923e3a8428395f69bfd30114e50773
ryan10328 <ryan10328@gmail.com>
2017-06-10 16:53:10 +0800

    任務 15：使用 Http 服務元件呼叫 RESTful APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { FooterComponent } from './footer/footer.component';
 import { Component, ViewChild } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/rx';
+import { Observable } from "rxjs/Observable";
+
+const BASE_URL: string = 'http://localhost:3000/todos';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +14,7 @@ import { Http } from '@angular/http';
 export class AppComponent {
 
   constructor(private http: Http) {
-
+    this.getTodos().subscribe(data => this.todos = data);
   }
 
   // @ViewChild('myfooter') footer: FooterComponent;
@@ -21,11 +25,20 @@ export class AppComponent {
   todos: any[] = [];
   isToggleAll: boolean = false;
 
+  getTodos(): Observable<any[]> {
+    return this.http.get(BASE_URL)
+      .map(data => data.json());
+  }
+
   addTodo() {
     // let input = evt.target as HTMLInputElement;
     if (this.todo) {
       //  this.todos.push(input.value);
-      this.todos = [...this.todos, { todo: this.todo, done: false }];
+      // this.todos = [...this.todos, { todo: this.todo, done: false }];
+
+      this.http.post(BASE_URL, { todo: this.todo, done: false })
+        .concatMap(data => this.getTodos())
+        .subscribe(data => this.todos = data);
 
       // input.value = '';
       this.todo = '';
@@ -35,7 +48,18 @@ export class AppComponent {
   }
 
   clearCompleted() {
-    this.todos = this.todos.filter(data => !data.done);
+    this.todos = this.todos.filter(data => data.done);
+
+    let obs: any[] = [];
+    this.todos.forEach(data => {
+      let req = this.http.delete(`${BASE_URL}/${data.id}`);
+      obs.push(req);
+    });
+
+    Observable.forkJoin(obs)
+      .concatMap(data => this.getTodos())
+      .subscribe(data => this.todos = data);
+
   }
 
   onSelectedType(evt) {
@@ -46,12 +70,33 @@ export class AppComponent {
   toggleAll() {
     this.todos = this.todos.map(item => {
       return item.done === this.isToggleAll ?
-        { todo: item.todo, done: item.done } :
-        { todo: item.todo, done: !item.done };
+        { todo: item.todo, done: item.done, id: item.id } :
+        { todo: item.todo, done: !item.done, id: item.id };
     });
+
+
+    let obs: any[] = [];
+    this.todos.forEach(data => {
+      let req = this.http.put(`${BASE_URL}/${data.id}`,
+        { todo: data.todo, done: data.done });
+      obs.push(req);
+    });
+
+    Observable.forkJoin(obs)
+      .concatMap(data => this.getTodos())
+      .subscribe(data => this.todos = data);
   }
 
   deleteTodo(item) {
-    this.todos = this.todos.filter(data => item != data);
+    // this.todos = this.todos.filter(data => item != data);
+    this.http.delete(`${BASE_URL}/${item.id}`)
+      .concatMap(data => this.getTodos())
+      .subscribe(data => this.todos = data);
+  }
+
+  toggleSingleTodo(item) {
+    this.http.put(`${BASE_URL}/${item.id}`, { done: item.done, todo: item.todo })
+      .concatMap(data => this.getTodos())
+      .subscribe(data => this.todos = data);
   }
 }
