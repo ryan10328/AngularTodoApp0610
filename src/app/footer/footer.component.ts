b3b1130cda0a5b494d2623a402ed9e37ab064296
ryan10328 <ryan10328@gmail.com>
2017-06-10 13:43:47 +0800

    任務 10-1：套用 @Input() 裝飾器 (Decorator)
    1. 自訂輸入屬性名稱
    2. 實作 OnChanges 介面，並實作 ngOnChanges 方法

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnChanges {
+  moreThanFive: boolean = false;
 
-  @Input() todos: any[] = [];
+  ngOnChanges() {
+    console.log('ngOnChanges');
+    if (this.myTodos.length >= 5) {
+      this.moreThanFive = true;
+    }
+
+  }
+
+  @Input('todos') myTodos: any[] = [];
 
   constructor() { }
 
