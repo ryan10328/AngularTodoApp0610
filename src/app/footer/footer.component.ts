6d870d35278f7b3d7d34427c92c0eaffa8f9a649
ryan10328 <ryan10328@gmail.com>
2017-06-10 13:48:26 +0800

    任務 10-2：套用 @Input() 裝飾器 (Decorator)
    移除 ngOnChanges() 方法，改用 TypeScript 的 getter/setter 來自訂 todos 的傳入方式

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit, OnChanges {
-  moreThanFive: boolean = false;
+export class FooterComponent implements OnInit {
+  // moreThanFive: boolean = false;
 
-  ngOnChanges() {
-    console.log('ngOnChanges');
-    if (this.myTodos.length >= 5) {
-      this.moreThanFive = true;
-    }
+  // ngOnChanges() {
+  //   console.log('ngOnChanges');
+  //   if (this.myTodos.length >= 5) {
+  //     this.moreThanFive = true;
+  //   }
 
+  // }
+
+  private _todos: any[] = [];
+
+  get todos() {
+    return this._todos;
   }
 
-  @Input('todos') myTodos: any[] = [];
+  @Input() set todos(value) {
+    this._todos = value;
+  }
 
   constructor() { }
 
