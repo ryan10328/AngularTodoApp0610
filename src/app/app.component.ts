88c5d2d9e2f5144c4b2ede7907445f4e2b676475
ryan10328 <ryan10328@gmail.com>
2017-06-10 10:16:57 +0800

    任務 05：套用屬性繫結 (Property Binding)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']  
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  inputHint: string = 'What needs to be done???';
+
 }
