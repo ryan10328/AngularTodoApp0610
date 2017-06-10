8624f822e5c9e20945467c317d2bf345c6e560fb
ryan10328 <ryan10328@gmail.com>
2017-06-10 11:25:46 +0800

    任務 06：套用事件繫結 (Event Binding)
    進階練習
    1. 透過 keyup.enter
    2. 當欄位沒有輸入任何資料直接按下 Enter 時，請不要新增空值到 todos 陣列中

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   inputHint: string = 'What needs to be done???';
   myColSpan: number = 2;
+  todos: any[] = [];
+
+  addTodo(evt: KeyboardEvent) {
+    let input = evt.target as HTMLInputElement;
+    if (input.value) {
+      // this.todos.push(input.value);
+      this.todos = [...this.todos, input.value];
+
+      input.value = '';
+    }
+  }
 }
